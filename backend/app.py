f8611334ff6d9d4d9c1d58260f2e02fb1907686e
zidane2001 <zidane2001@github.com>
2025-11-10 14:49:30 +0100

    push

--- backend/app.py
+++ backend/app.py
@@ -29,11 +29,7 @@ def handle_preflight():
 
 @app.after_request
 def add_cors_headers(response):
-    origin = request.headers.get('Origin')
-    if origin:
-        response.headers.add('Access-Control-Allow-Origin', origin)
-    else:
-        response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', 'https://woodshot-frontend.onrender.com')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token,Accept,Accept-Version,Content-Length,Content-MD5')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
